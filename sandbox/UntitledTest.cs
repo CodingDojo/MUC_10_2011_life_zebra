d8bf70cb5d89f2b8bc63421e587549376add929b
root <root@rails.(none)>
2011-10-17 21:31:36 +0000

    14

--- sandbox/UntitledTest.cs
+++ sandbox/UntitledTest.cs
@@ -13,5 +13,15 @@ public class UntitledTest
 
         Assert.AreEqual(0, game.LiveCellsCount());
     }
+
+    public void TestCellWithMoreThanCellLiveNeighboursDies() {
+        Game game = new Game(
+            5,5, new List<string>() 
+                {"0,0", "0,1", "1,0", "1,1","2,0","2,1"});
+
+        game.Evolve();
+
+        Assert.AreEqual(4, game.LiveCellsCount());
+    }
 }
 
