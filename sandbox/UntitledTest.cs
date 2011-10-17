dd76e62b5ad1835822322ae9d5e5123633ed97c2
root <root@rails.(none)>
2011-10-17 22:12:47 +0000

    39

--- sandbox/UntitledTest.cs
+++ sandbox/UntitledTest.cs
@@ -14,26 +14,26 @@ public class UntitledTest
         Assert.AreEqual(0, game.LiveCellsCount());
     }
 
-    
+    [Test]
     public void TestCellWithMoreThanCellLiveNeighboursDies() {
         Game game = new Game(
             5,5, new List<string>() 
                 {"0,0", "1,0", "1,1","2,0","2,1"});
 
         game.Evolve();
-        //game.PrintTable();
+        game.PrintTable();
 
         Assert.AreEqual(4, game.LiveCellsCount());
     }
+
    [Test]
     public void TestCellWithThreeLiveNeighboursRespawns() {
         Game game = new Game(
             5,5, new List<string>() 
                 {"0,0", "0,1", "1,0"});
 
-        game.PrintTable();
+
         game.Evolve();
-        game.PrintTable();
 
         Assert.AreEqual(4, game.LiveCellsCount());
     }
