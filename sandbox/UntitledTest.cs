bc61d4610cc5926071b862737539e812d7a35936
root <root@rails.(none)>
2011-10-17 21:52:22 +0000

    26

--- sandbox/UntitledTest.cs
+++ sandbox/UntitledTest.cs
@@ -29,7 +29,7 @@ public class UntitledTest
     public void TestCellWithThreeLiveNeighboursRespawns() {
         Game game = new Game(
             5,5, new List<string>() 
-                {"0,0", "1,0", "0,1"});
+                {"0,0", "0,1", "1,0"});
 
         game.Evolve();
         game.PrintTable();
