b00829596671fae36eae81b71386e0211af85a71
root <root@rails.(none)>
2011-10-17 21:24:56 +0000

    12

--- sandbox/UntitledTest.cs
+++ sandbox/UntitledTest.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 [TestFixture]
 public class UntitledTest
 {
+    [Test]
     public void TestOneCellDiesAfterEvolution() {
         Game game = new Game(5,5, new List<string>() {"0,0"});
 
